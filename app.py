2dc280bc49f8c3cba4a32a2b94b79ea3d3aa39a2
Eswari <158579538+Aanshies@users.noreply.github.com>
2025-06-27 14:22:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ import streamlit as st
 from resume_parser import extract_text_from_pdf, extract_keywords, match_keywords
 import spacy
 
+
 try:
     nlp = spacy.load("en_core_web_sm")
-except OSError:
-    from spacy.cli import download
-    download("en_core_web_sm")
+except:
+    import os
+    os.system("python -m spacy download en_core_web_sm")
     nlp = spacy.load("en_core_web_sm")
 
 
