21a2ff04558bd598bdd42cd63f24842209e920e1
Eswari <158579538+Aanshies@users.noreply.github.com>
2025-06-28 10:29:21 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,10 +1,3 @@
-#!/usr/bin/env bash
-
-# Install dependencies first
+pip install --upgrade pip
 pip install -r requirements.txt
 set -o errexit
-
-pip install --upgrade pip
-
-# Then download the spacy model
-python -m spacy download en_core_web_sm
