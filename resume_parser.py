566b7e1155dbe5884eee1f6664fe16c4c11a8e6a
Eswari <158579538+Aanshies@users.noreply.github.com>
2025-06-28 11:08:32 +0530

    Update resume_parser.py

--- resume_parser.py
+++ resume_parser.py
@@ -9,7 +9,7 @@ try:
     nlp = spacy.load("en_core_web_sm")
 except:
     import os
-    os.system("python -m spacy download en_core_web_sm")
+    
     nlp = spacy.load("en_core_web_sm")
 
 def extract_text_from_pdf(file):
